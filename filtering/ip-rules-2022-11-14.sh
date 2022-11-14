7f691d66a4b94991a47ba0649a5bdb59a3cb2d82
Imran Mohamed <imran@example.com>
2022-11-14 18:09:34 +0530

    restrictive input output chains only allowing defined protocols and ports

--- filtering/ip-rules-2022-11-14.sh
+++ filtering/ip-rules-2022-11-14.sh
@@ -1,17 +1,24 @@
 # Drop the incoming packets
 -P INPUT DROP
+# Since default outgoing packets blocked, allow only establed connections into INPUT chain
+-A OUTPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 
 # Drop the forwarding packets
 -P FORWARD DROP
 
-# Allow to access outside without conditions
--P OUTPUT ACCEPT
+# Drop outside packets
+-P OUTPUT DROP
 # Since default incoming packets blocked, allow only established connections from OUTPUT chain
 -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 
-# Allow incoming SSH connections
+# Allow SSH connections
 -A INPUT -p tcp -m tcp --dport 22 -j ACCEPT
+-A OUTPUT -p tcp -m tcp --dport 22 -j ACCEPT
+
+# Allow dns UDP
+-A OUTPUT -p udp -m udp --dport 53 -j ACCEPT
 
 # Allow incoming/forwarding ICMP ping requests
 -A INPUT -p icmp -j ACCEPT
 -A FORWARD -p icmp -j ACCEPT
+-A OUTPUT -p icmp -j ACCEPT
